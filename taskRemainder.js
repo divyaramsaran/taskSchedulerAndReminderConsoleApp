a4de3f91eb9e6d72556c23aeabdffb9fc441ba65
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-06-24 11:39:30 +0000

    adds addTask function

--- taskRemainder.js
+++ taskRemainder.js
@@ -1,9 +1,22 @@
+const addTask = () => {
+  console.log("1.Normal Task\n2.Recurring Task");
+  const taskType = Number(prompt("Enter task type"));
+  if (taskType > 2 || taskType < 1) {
+    console.log("Enter A Valid Task Type");
+    return addTask();
+  }
+  const description = prompt("Enter task description");
+  const time = Number(prompt("Enter time in (Seconds):"));
+  return taskType === 1 ? "normalTask" : "recurringTask";
+};
+
 const commands = () => {
+  const operations = [addTask];
   console.log(
     "1.Add Task\n2.List Tasks\n3.Cancel Reminder\n4.Mark as Completed\n5.Exit"
   );
   const choice = Number(prompt("Enter your choice:"));
-  return choice;
+  return operations[choice - 1]();
 };
 
 console.log(commands());
